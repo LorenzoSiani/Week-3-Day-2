e427e104cbfa2b37e7a812b73112e54f2b511a20
Lorenzo <lorenzosiani43@gmail.com>
2023-07-04 16:57:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputNameField = document.getElementById('input-name')
+const sendButton = document.getElementById('send')
+const emptyButton = document.getElementById('empty')
+const listSavedItems = document.querySelector('ul')
+
+let saveValue = function () {
+    let inputField = inputNameField.value 
+    localStorage.setItem ("name", inputField)
+    const newLi = document.createElement("li")
+    listSavedItems.appendChild(newLi)
+}
